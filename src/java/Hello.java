9f8b007ffa3a956c51ed41216ff675c714d84c4f
annelirasu <annelirasu@gmail.com>
2012-12-09 14:53:48 +0200

    Andmebaas autoincrements, gitignores asu juures

--- src/java/Hello.java
+++ src/java/Hello.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -35,14 +34,14 @@ public class Hello extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Hello</title>");            
+            out.println("<title>Servlet Hello</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet Hello at " + request.getContextPath() + "</h1>");
             out.println("Midagi muud");
             out.println("</body>");
             out.println("</html>");
-        } finally {            
+        } finally {
             out.close();
         }
     }
