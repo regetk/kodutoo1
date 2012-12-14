8279574f5dd1b5037cb19fc47f5785832759e568
annelirasu <annelirasu@gmail.com>
2012-12-14 22:06:50 +0200

    Admin_Liigi_redaktori alge vigade kontrolliga

---
+++ src/java/adm_liigi_redaktor.java
@@ -0,0 +1,181 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author dell
+ */
+public class adm_liigi_redaktor extends HttpServlet {
+
+    @Override
+    public void init() {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet adm_liigi_redaktor</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet adm_liigi_redaktor at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+          
+                
+        showForm(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+         List<String> errors = getValidationErrors(request);
+	        if (!errors.isEmpty()) {
+	            request.setAttribute("errors", errors);
+	            showForm(request, response);
+	            return;
+	        }
+               
+                
+        //POSTist saadaolevad parameetrid.        
+	String kood =request.getParameter("ay_liik_kood");
+        String nimi =request.getParameter("ay_liik_nimi"); 
+        String komm =request.getParameter("ay_liik_komm"); 
+        
+        String ylem_id =request.getParameter("ay_liik_ylemus");
+        
+        Paring p=new Paring();
+        ArrayList params=new ArrayList();
+        params.add(kood);
+        params.add(nimi);
+        params.add(komm);
+                      
+        String sql="INSERT INTO RIIGI_ADMIN_YKSUSE_LIIK  VALUES (null,'An4', CURRENT_DATE,'Nobody','9999-12-31', NULL,'9999-12-31',?,?,?,CURRENT_DATE,'9999-12-31')";
+        p.DMLParing(sql, params);
+        
+        //lisame nyyd VOIMALIK_Alluvus
+       
+        //asume hankima viimati sisestatud ay liigi ID-d
+        ArrayList param2=new ArrayList();
+        param2.add(kood);
+        String sql2 = "SELECT riigi_admin_yksuse_lik_id  FROM RIIGI_ADMIN_YKSUSE_LIIK WHERE KOOD=?";
+        Object res[][] = p.SelectParing(sql2, param2);
+        String alluv_id = res[0][0].toString();
+        
+        
+         ArrayList param3=new ArrayList();
+         param3.add(ylem_id);
+         param3.add(alluv_id);
+         String sql3 = "INSERT INTO VOIMALIK_ALLUVUS  VALUES (null,'An4', CURRENT_DATE,'Nobody','9999-12-31', NULL,'9999-12-31',?,?,'komm')";
+         p.DMLParing(sql3, param3);
+         showForm(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private void showForm(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        
+        //ylemused
+
+        Paring p = new Paring();
+        String sql = "SELECT riigi_admin_yksuse_lik_id,nimetus FROM RIIGI_ADMIN_YKSUSE_LIIK";
+        Object tulem[][] = p.SelectParing(sql, new ArrayList());
+        request.setAttribute("vormiAndmed", tulem);
+        //SQL alluvus
+
+       
+       
+        
+        
+        String sql2 = "SELECT NIMETUS FROM RIIGI_ADMIN_YKSUSE_LIIK RIIGI_ADMIN_YKSUSE_LIIK WHERE RIIGI_ADMIN_YKSUSE_LIK_ID NOT IN (SELECT VOIMALIK_ALLUV_LIIK_ID FROM VOIMALIK_ALLUVUS";
+        
+        request.getRequestDispatcher("adm_liigi_redaktor.jsp").forward(request, response);
+    } 
+
+    //meetod andmete õigsuse kontrolliks ja teated, kui miskit on valesti
+    private List<String> getValidationErrors(HttpServletRequest request) {
+        List<String> errors = new ArrayList<String>();
+        if ("".equals(request.getParameter("ay_liik_kood"))) {
+            errors.add("Sisesta alluvusyksuse liigi kood!");
+        }
+        if ("".equals(request.getParameter("ay_liik_nimi"))) {
+            errors.add("Sisesta alluvusyksuse liigi nimi!");
+        }
+        
+            if ("".equals(request.getParameter("ay_liik_komm"))) {
+            errors.add("Sisesta alluvusyksuse liigi kommentaar!");
+        }
+                if ("".equals(request.getParameter("ay_liik_nimi"))) {
+            errors.add("Sisesta alluvusyksuse liigi nimi!");
+        }
+
+        return errors;
+    }
+}
--- web/adm_liigi_redaktor.jsp
+++ web/adm_liigi_redaktor.jsp
@@ -5,6 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,36 +14,56 @@
     </head>
     <body>
         <h1>Administratiivüksuste liigi redaktor</h1>
+        <!--veateated-->
+    <c:if test="${not empty errors}">
+        <div style="color:red">
+            <c:forEach var="error" items="${errors}">
+                <c:out value="${error}"></c:out><br />
+            </c:forEach>
+        </div>
+        <br/><br/>
+    </c:if>
 
-        <table >
-            <form method="POST">
-                <tr >
-                    <td bgcolor="silver">
-                        Kood: <input name="ad_yks_liik_kood" value="${param.ad_yks_liik_kood}"><br/>
-                        Nimetus: <input name="ad_yks_liik_nim" value="${param.ad_yks_liik_nim}"><br/>
-                        Kommentaar: <br/>
-                        <textarea name='textAreaName' id='textAreaId'>Kommentaar riigi_admin_yksuse_liik baasist
-                        </textarea><br/>
-                        Allub: <select name="ad_yks_liik_valik">
-                            <c:forEach var="entry" items="${formData.ad_yks_liigid}">
-                                <c:set var="selected" value="" />
-                                <c:if test="${entry.key == param.ad_yks_liik_id}">
-                                    <c:set var="selected" value="selected=\"selected\"" />
-                                </c:if>
-                                <option value="${entry.key}" ${selected}>${entry.value}</option>
-                            </c:forEach> <br/>
+    <table >
+        <form method="POST">
+            <tr >
+                <td bgcolor="silver">
+                    Kood: <input name="ay_liik_kood" value=""><br/>
+                    Nimetus: <input name="ay_liik_nimi" value=""><br/>
+                    Kommentaar: <br/>
+                    <textarea name='ay_liik_komm' id='textAreaId'>Kommentaar 
+                    </textarea><br/>
+                    Allub: <select name="ay_liik_ylemus"
+                                   <%
+                                       Object items = request.getAttribute("vormiAndmed");
+                                       if (items != null) {
+                                           //String [] arr=(String[])items;
+                                           Object obs[][] = (Object[][]) items;
+                                           //out.print(items);
 
-                    </td>
-                    <td valign="top" bgcolor="#FFCCFF">Alluvad<br/>
-                        <input type=TEXT name="muutuja_allujate_kollektsioonist" value="Allujate_kollekts_isend" > <!-- <//%=textFields[j]%>  -->
-                        <input type="submit" name="save_ad_liik" value="Eemalda">
-                    </td>
-                
-                <tr  bgcolor="#99CCFF" >
+                                           for (int a = 0; a < obs.length; a++) {
+                                               String tt = obs[a][0].toString();
+                                               String it = obs[a][1].toString();
+                                               out.println("<option value='" + tt + "'>" + it + "</option>");
+                                           }
+                                       }
+                                   %>
+                                   <option value=''>uu</option>
+                    </select>
+                    <br/>
+
+                </td>
+                <td valign="top" bgcolor=""silver">Alluvad<br/>
+                    <input type=TEXT name="muutuja_allujate_kollektsioonist" value="Allujate_kollekts_isend" > <!-- <//%=textFields[j]%>  -->
+                    <input type="submit" name="save_ad_liik" value="Eemalda">
                    
-                    <td colspan="2" align="right"><input type="submit" name="save_ad_liik" value="Salvesta"><input type="submit" name="cancel_ad_liik" value="Loobu"></td>
-                </tr>
-            </form>
-        </table>
-    </body>
+                </td>
+
+            <tr  bgcolor="#99CCFF" >
+
+                <td colspan="2" align="right"><input type="submit" name="save_ad_liik" value="Salvesta"><input type="submit" name="cancel_ad_liik" value="Loobu"></td>
+            </tr>
+        </form>
+    </table>
+</body>
 </html>
